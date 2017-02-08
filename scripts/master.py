b61ea47491a978d26fc3ac03d2dc8b8ad912a4d5
Criticalstone <kevinhoog@gmail.com>
2017-02-08 14:00:12 +0100

    Changes topics on which we listen and publishes

--- scripts/master.py
+++ scripts/master.py
@@ -8,13 +8,13 @@ class TruckMaster:
     def __init__(self):
         self.go = False
         self.manual = True
-        self.pub = rospy.Publisher('ackermann_control', AckermannDrive, queue_size=10)
+        self.pub = rospy.Publisher('truck_cmd', AckermannDrive, queue_size=10)
 
         rospy.init_node('truckmaster', anonymous=False)
         rospy.Subscriber('aut_ackermann_control', AckermannDrive, self.autAckermannHandler)
         rospy.Subscriber('man_ackermann_control', AckermannDrive, self.manualAckermannHandler)
         rospy.Subscriber('manual_control', Bool, self.manualHandler)
-        rospy.Subscriber('go', Bool, self.goHandler)
+        rospy.Subscriber('dead_mans_grip', Bool, self.goHandler)
 
     def autAckermannHandler(self,data):
         if not self.manual and self.go:
