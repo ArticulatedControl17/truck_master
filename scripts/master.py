2befde160729eff142bed0e03a99d96311271af3
Lars Niklasson <larsniklasson94@gmail.com>
2017-02-11 21:09:27 +0100

    change names

--- scripts/master.py
+++ scripts/master.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# license removed for brevity
 import rospy
 from hw_api_ackermann.msg import AckermannDrive
 from std_msgs.msg import Bool
@@ -8,23 +7,20 @@ class TruckMaster:
     def __init__(self):
         self.go = False
         self.manual = True
-        self.pub = rospy.Publisher('truck_cmd', AckermannDrive, queue_size=10)
+        self.pub = rospy.Publisher('master_drive', AckermannDrive, queue_size=10)
 
         rospy.init_node('master', anonymous=False)
-        rospy.Subscriber('aut_ackermann_control', AckermannDrive, self.autAckermannHandler)
-        rospy.Subscriber('man_ackermann_control', AckermannDrive, self.manualAckermannHandler)
-        rospy.Subscriber('manual_control', Bool, self.manualOrAutomaticHandler)
-        rospy.Subscriber('dead_mans_grip', Bool, self.goHandler)
+        rospy.Subscriber('auto_drive', AckermannDrive, self.autAckermannHandler)
+        rospy.Subscriber('man_drive', AckermannDrive, self.manualAckermannHandler)
+        rospy.Subscriber('auto_ctrl', Bool, self.manualOrAutomaticHandler)
+        rospy.Subscriber('dead_mans_switch', Bool, self.goHandler)
 
     def autAckermannHandler(self,data):
         if not self.manual and self.go:
             self.pub.publish(data)
 
     def manualAckermannHandler(self,data):
-        print "manual", self.manual
-        print "go", self.go
         if self.manual and self.go:
-            print "publishing data", data
             self.pub.publish(data)
 
     def manualOrAutomaticHandler(self,data):
@@ -39,8 +35,5 @@ class TruckMaster:
             self.pub.publish(ack)
 
 if __name__ == '__main__':
-    try:
-        tm = TruckMaster()
-        rospy.spin()
-    except rospy.ROSInterruptException:
-        pass
+    tm = TruckMaster()
+    rospy.spin()
