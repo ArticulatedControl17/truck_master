6e015e87e2e818183b79ab31e2ca10f6c9f9e388
larsniklasson <larsniklasson94@gmail.com>
2018-01-21 18:38:27 +0100

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -4,7 +4,7 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
-sudo apt-get install ros-kinetic-desktop
+sudo apt-get install -y ros-kinetic-desktop
 sudo rosdep init
 rosdep update
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
@@ -24,8 +24,8 @@ cd src
 
 # ----get dependencies-----
 sudo apt-get update
-sudo apt-get install ros-kinetic-ackermann-msgs
-sudo apt-get install ros-kinetic-joy
+sudo apt-get install -y ros-kinetic-ackermann-msgs
+sudo apt-get install -y ros-kinetic-joy
 
 #------clone needed repositories-----
 
